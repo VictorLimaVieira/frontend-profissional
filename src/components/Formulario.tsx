c69196c548ef47332050c17a1188e839b74b67ea
Victor Lima Vieira <victorlvminhaconta@gmail.com>
2026-02-09 11:24:33 -0300

    apliquei o ZOD no front-end enviando erros ao backend

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -12,22 +12,46 @@ export function Formulario({ aoCadastrar }: FormularioProps) {
     const [nome, setNome] = useState('');
     const [email, setEmail] = useState('');
 
-    async function handleSubmit(event: any) {
-        event.preventDefault();
+   // Novo estado: para guardar a mensagem de erro (se houver)
+   const [erro, setErro] = useState('');
+   
+   async function handleSubmit(event:any) {
+    event.preventDefault();
+    setErro(''); // limpa erros antigos ao tentar de novo
+        try {
+            // tentativa: o código tenta fazer o post
+            const resposta = await api.post('/usuarios', {
+                nome: nome,
+                email: email
+            });
 
-        const resposta = await api.post('/usuarios', {
-            nome: nome,
-            email: email
-        });
+            // se chegou aqui deu certo (201 created)
+            aoCadastrar(resposta.data);
+            setNome('');
+            setEmail('');
+            alert('Usuário cadastrado com sucesso!');
 
-        // 3. USO: Aqui a gente chama a função do Pai!
-        aoCadastrar(resposta.data); 
+        } catch (err: any) {
+            // captura: se o backend devolver 400 ou 500, cai aqui
 
-        setNome('');
-        setEmail('');
-        alert('Usuário cadastrado com sucesso!');
-        // Note que NÃO tem mais o reload aqui!
-    }
+            // verifica se é erro do Zod (que tem "detalhes")
+            if (err.response?.data?.detalhes) {
+                // pega o primeiro erro da lista (ex: "o nome precisa ter 3 letras")
+                const errosDoZod = err.response.data.detalhes;
+                const primeiroErro = Object.values(errosDoZod)[0];
+                setErro(primeiroErro as string);
+            }
+            // verifica se é erro genérico (ex: EMAIL JÁ EXISTE)
+            else if (err.response?.data?.error) {
+                setErro(err.response.data.error);
+            }
+            // se o servidor estiver desligado
+            else {
+                setErro("Erro ao conectar com o servidor.");
+            }
+        }
+    
+   }
 
     return (
         <form onSubmit={handleSubmit}>
@@ -41,7 +65,11 @@ export function Formulario({ aoCadastrar }: FormularioProps) {
                 value={email}
                 onChange={e => setEmail(e.target.value)}
             />
+            {/* Exibição: só mostra essa linha se tiver erro  */}
+            {erro && <p style={{ color: 'red', marginTop: '5px' }}>{erro}</p>}
+
             <button type="submit">Cadastrar</button>
         </form>
     )
 }
+ 
